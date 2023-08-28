1e55225d0be91bf2a7ebe981239c969627ce7d46
Chigozie Peter <ujahchigozie@gmail.com>
2023-08-28 13:31:30 +0800

    Update user.php

--- resources/views/user.php
+++ resources/views/user.php
@@ -5,7 +5,7 @@
     <title>Luminova User</title>
 </head>
 <body>
-	<h1>Welcome {<?php echo $this->name;?>}</h1>
-    <a href="<?php echo $this->base;?>">Home</a>
+	<h1>Welcome {<?=$this->name;?>}</h1>
+    <a href="<?=$this->base;?>">Home</a>
 </body>
 </html>
